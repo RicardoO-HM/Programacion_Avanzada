4b1cabb7fe3740ac8ba845a0d8f761e8b6135a75
Iliev <iliev.p.h2608@gmail.com>
2026-09-17 10:08:39 -0600

    Catalogo

--- Catalogo.py
+++ Catalogo.py
@@ -1,17 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 17 09:09:25 2026
-
-@author: Iliev
-"""
-
-def catalogo() ->dict:
+def catalogo() -> dict:
     """Carga el catalogo de los productos disponibles, y lo declaramos
        como diccionario   
     """
-    return{
-        "P001" : {"Nombre" : "Cafe", "Precio" : 45.0, "Stock" : 20}, 
-        "P002" : {"Nombre" : "Te", "Precio" : 30.0, "Stock" : 15},
-        "P003" : {"Nombre" : "Galletas",  "Precio" : 25.0, "Stock" : 15}
+    return {
+        "P001": {"nombre": "Cafe", "precio": 45.0, "stock": 20},
+        "P002": {"nombre": "Te", "precio": 30.0, "stock": 15},
+        "P003": {"nombre": "Galletas", "precio": 25.0, "stock": 15}
         #Se podrian agregar mas productos    
         }
+
+def mostrar_catalogo(catalogo):
+    """Imprime una tabla con los productos disponibles: id, nombre,
+    precio y stock actual.
+    """
+    print(f"{'ID':<6}{'Producto':<15}{'Precio':<10}{'Stock':<6}")
+    for id_producto, datos in catalogo.items():
+        print(f"{id_producto:<6}{datos['nombre']:<15}{datos['precio']:<10.2f}{datos['stock']:<6}")
